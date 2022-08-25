4f76b374897beb9f701f366a6c9f0b26c3f5a941
Ryan Keegan <ryan-thomaskeegan@hotmail.com>
2022-08-25 01:47:39 -0400

    Add computer choice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,8 @@
-console.log("Hello");
+
+function getComputerSelection() {
+    const choiceOptions = ['Rock','Paper','Scissors'];
+    const rng = Math.floor(Math.random() * 3);
+
+    return computerSelection[rng];
+}
+
